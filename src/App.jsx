fa526b559ec5888f47904339f8f729395692dd93
Soh1121 <satou.shg@gmail.com>
2025-07-28 06:43:57 +0900

    ✨feat: カウントアップボタンを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./styles.css";
 
 export default function App() {
+    console.log("App");
+    const [count, setCount] = useState(0);
+
+    const onClickCountUp = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div className="App">
             <h1>Hello, React!</h1>
             <p>Welcome to your React application.</p>
+            <p>{count}</p>
+            <button type="button" onClick={onClickCountUp}>カウントアップ</button>
         </div>
     );
 }
