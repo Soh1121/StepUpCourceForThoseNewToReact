bba9eb643b96f8e843e99a64c4a79ebbdb685944
Soh1121 <satou.shg@gmail.com>
2025-07-28 06:40:51 +0900

    ✨feat: 環境構築

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { BrowserRouter, Link } from "react-router-dom";
-
-import { Router } from "./router/Router";
+import React from 'react';
 import "./styles.css";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Link to="/">Home</Link>
-                <br />
-                <Link to="/page1">Page1</Link>
-                <br />
-                <Link to="/page2">Page2</Link>
-            </div>
-            <Router />
-        </BrowserRouter>
+        <div className="App">
+            <h1>Hello, React!</h1>
+            <p>Welcome to your React application.</p>
+        </div>
     );
 }
