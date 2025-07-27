4e9569afa8806250a262a22cd82e97e13a3f3103
Soh1121 <satou.shg@gmail.com>
2025-07-28 06:54:40 +0900

    ✨feat: 再レンダリングの学習の準備

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import "./styles.css";
 
 export default function App() {
     console.log("App");
-    const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
+    const [open, setOpen] = useState(false);
 
-    const onClickCountUp = () => {
-        setCount(count + 1);
-    }
+    const onChangeText = (e) => setText(e.target.value);
+
+    const onClickOpen = () => setOpen(!open);
 
     return (
         <div className="App">
-            <input type="text" />
+            <input type="text" value={text} onChange={onChangeText} />
             <br />
             <br />
-            <button type="button">表示</button>
-            <ChildArea />
+            <button type="button" onClick={onClickOpen}>表示</button>
+            <ChildArea open={open} />
         </div>
     );
 }
