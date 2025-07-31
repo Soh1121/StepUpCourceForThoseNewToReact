de833a82b74d01cc72024d83ee2a509bd2c2c0c8
Soh1121 <satou.shg@gmail.com>
2025-08-01 06:47:04 +0900

    ✨feat: React Router の基本を学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
 import "./styles.css";
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>Hello, React!</h1>
-            <p>Welcome to your React application.</p>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Link to="/">Home</Link>
+                <br />
+                <Link to="/page1">Page1</Link>
+                <br />
+                <Link to="/page2">Page2</Link>
+            </div>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/page1">
+                    <Page1 />
+                </Route>
+                <Route path="/page2">
+                    <Page2 />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
