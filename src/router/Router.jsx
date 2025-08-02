78c35e7546e1c5961d6ca0ed7f1a7f06c139fb03
Soh1121 <satou.shg@gmail.com>
2025-08-02 21:56:38 +0900

    ✨feat: Route の設定の切り出しを学習

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import { Page1 } from "../Page1";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
 import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
 
 export const Router = () => {
     return (
@@ -15,15 +16,15 @@ export const Router = () => {
                 path="/page1"
                 render={({ match: { url } }) => (
                     <Switch>
-                        <Route exact path={url}>
-                            <Page1 />
-                        </Route>
-                        <Route exact path={`${url}/detailA`}>
-                            <Page1DetailA />
-                        </Route>
-                        <Route exact path={`${url}/detailB`}>
-                            <Page1DetailB />
-                        </Route>
+                        {page1Routes.map((route) => (
+                            <Route
+                                key={route.path}
+                                exact={route.exact}
+                                path={`${url}${route.path}`}
+                            >
+                                {route.children}
+                            </Route>
+                        ))}
                     </Switch>
                 )}
             />
