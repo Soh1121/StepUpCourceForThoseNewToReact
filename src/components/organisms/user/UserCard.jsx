2013740925bc2289e4394ca183900fa3c45bfc16
Soh1121 <satou.shg@gmail.com>
2025-08-05 13:49:04 +0900

    ✨feat: UserCard の雛形の作成

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+    return (
+        <div>
+            <img src="" alt="プロフィール" />
+            <p>名前</p>
+            <dl>
+                <dt>メール</dt>
+                <dd>1111@example.com</dd>
+                <dt>TEL</dt>
+                <dd>090-0000-0000</dd>
+                <dt>会社名</dt>
+                <dd>ああああ株式会社</dd>
+                <dt>WEB</dt>
+                <dd>example.com</dd>
+            </dl>
+        </div>
+    );
+}
