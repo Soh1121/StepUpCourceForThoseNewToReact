4db658bad2214051ef825ae682079dbe6fdd4e31
Soh1121 <satou.shg@gmail.com>
2025-08-07 06:48:06 +0900

    ✨feat: page を作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import React from 'react';
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
 import "./styles.css"
-import { BrowserRouter } from 'react-router-dom';
-import { DefaultLayout } from './components/templates/DefaultLayout';
-
-const user = {
-    name: "Soh",
-    image: "https://images.unsplash.com/photo-1737309625096-4c470d65b484?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    email: "12345@example.com",
-    phone: "090-0000-0000",
-    company: {
-        name: "テスト株式会社"
-    },
-    website: "https://google.com"
-}
+import { Router } from './router/Router';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <DefaultLayout>
-                <PrimaryButton>テスト</PrimaryButton>
-                <SecondaryButton>検索</SecondaryButton>
-                <br />
-                <SearchInput />
-                <UserCard user={user} />
-            </DefaultLayout>
-        </BrowserRouter>
+        <Router />
     );
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+import { SearchInput } from "../molecules/SearchInput"
+import { UserCard } from "../organisms/user/UserCard"
+
+const users = [...Array(10).keys()].map((val) => {
+    return {
+        id: val,
+        name: `Soh${val}`,
+        image: "https://images.unsplash.com/photo-1737309625096-4c470d65b484?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        email: "12345@example.com",
+        phone: "090-0000-0000",
+        company: {
+            name: "テスト株式会社"
+        },
+        website: "https://google.com"
+    };
+});
+
+export const Users = () => {
+    return (
+        <SContainer>
+            <h2>ユーザー一覧</h2>
+            <SearchInput />
+            <SUserArea>
+                {users.map((user) => (
+                    <UserCard key={user.id} user={user} />
+                ))}
+            </SUserArea>
+        </SContainer>
+    );
+};
+
+const SContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+`;
+
+const SUserArea = styled.div`
+    padding-top: 40px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 20px
+`;
