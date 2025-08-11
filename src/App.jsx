8236861a0443177fb0872dadab997296e34c8a3b
Soh1121 <satou.shg@gmail.com>
2025-08-12 06:49:41 +0900

    ✨feat: JSONPlaceholder からデータを取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import "./styles.css";
+import axios from 'axios';
 
 export default function App() {
+    const onClickUsers = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+            console.log(res.data);
+        }).catch((err) => console.log(err));
+    };
+    const onClickUser1 = () => {
+        axios.get("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+            console.log(res.data);
+        }).catch((err) => console.log(err));
+    };
     return (
         <div className="App">
-            <h1>Hello, React!</h1>
-            <p>Welcome to your React application.</p>
+            <button type="button" onClick={onClickUsers}>users</button>
+            <button type="button" onClick={onClickUser1}>id=1のuser</button>
         </div>
     );
 }
