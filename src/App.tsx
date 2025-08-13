5fdabae8fd901af4a6df4939a2cc5dd2828022f9
Soh1121 <satou.shg@gmail.com>
2025-08-14 06:41:41 +0900

    ✨feat: 環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { UserCard } from "./components/UserCard";
 import "./styles.css";
-import { UserProfile } from "./types/UserProfile";
-import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-    const { getUsers, userProfiles, loading, error } = useAllUsers();
-
-    const onClickFetchUser = () => getUsers();
-
     return (
         <div className="App">
-            <button type="button" onClick={onClickFetchUser}>データ取得</button>
-            <br />
-            {error ? (
-                <p style={{ color: "red" }}>データの取得に失敗しました</p>
-            ) : loading ? (
-                <p>Loading...</p>
-            ) : (
-                <>
-                    {userProfiles.map((user: UserProfile) => (
-                        <UserCard key={user.id} user={user} />
-                    ))}
-                </>
-            )}
+            <h1>Hello, React!</h1>
+            <p>Welcome to your React application.</p>
         </div>
     );
 }
