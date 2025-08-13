d407ad72423efa7a776e026e12ca2ca575705918
Soh1121 <satou.shg@gmail.com>
2025-08-14 06:41:41 +0900

    ✨feat: 環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import axios from "axios";
 import "./styles.css";
-import { useState } from "react";
-import { Todo } from "./Todo";
-import { TodoType } from "./types/todo";
-import { Text } from "./Text";
-import { UserProfile } from "./UserProfile";
-
-const user = {
-    name: "Soh",
-    // hobbies: ["ゲーム", "筋トレ"],
-};
 
 export default function App() {
-    const [todos, setTodos] = useState<Array<TodoType>>([]);
-
-    const onClickFetchData = () => {
-        axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res) => {
-            setTodos(res.data);
-        });
-    }
     return (
         <div className="App">
-            <UserProfile user={user} />
-            <Text color="red" fontSize="18px" />
-            <button type="button" onClick={onClickFetchData}>データ取得</button>
-            {todos.map((todo) => (
-                <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
-            ))}
+            <h1>Hello, React!</h1>
+            <p>Welcome to your React application.</p>
         </div>
     );
 }
