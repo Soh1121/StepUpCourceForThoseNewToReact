1ef2bf55e2abae19be10312fde70e80702659eb4
Soh1121 <satou.shg@gmail.com>
2025-08-15 13:54:44 +0900

    🐞fix: keyを追加

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
         <div className="App">
             <button type="button" onClick={onClickFetchData}>データ取得</button>
             {todos.map((todo) => (
-                <Todo title={todo.title} userId={todo.userId} completed={todo.completed} />
+                <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
             ))}
         </div>
     );
