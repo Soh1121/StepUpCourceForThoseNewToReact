c37a1bcb486f89aab74bc0bb216c4b42ee18e4dc
Soh1121 <satou.shg@gmail.com>
2025-08-18 06:59:53 +0900

    ✨feat: ユーザー情報を外部から取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import axios from "axios";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/UserProfile";
 
 const user = {
     id: 1,
@@ -9,11 +13,26 @@ const user = {
 };
 
 export default function App() {
-    const on
+    const [userProfiles, setUserProfile] = useState<Array<UserProfile>>([]);
+
+    const onClickFetchUser = () => {
+        axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
+            const data = res.data.map((user: User): UserProfile => ({
+                id: user.id,
+                name: `${user.name}(${user.username})`,
+                email: user.email,
+                address: `${user.address.city} ${user.address.suite} ${user.address.street}`,
+            }));
+            setUserProfile(data);
+        });
+    };
 
     return (
         <div className="App">
-            <UserCard user={user} />
+            <button type="button" onClick={onClickFetchUser}>データ取得</button>
+            {userProfiles.map((user: UserProfile) => (
+                <UserCard key={user.id} user={user} />
+            ))}
         </div>
     );
 }
