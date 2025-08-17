7b08f2dc9c2fb1b045938157510fdb1a27b9a171
Soh1121 <satou.shg@gmail.com>
2025-08-18 07:05:39 +0900

    ✨feat: loading と error を実装

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,14 @@ import "./styles.css";
 import { User } from "./types/api/user";
 import { UserProfile } from "./types/UserProfile";
 
-const user = {
-    id: 1,
-    name: "Soh",
-    email: "123@example.com",
-    address: "ADDRESS"
-};
-
 export default function App() {
     const [userProfiles, setUserProfile] = useState<Array<UserProfile>>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     const onClickFetchUser = () => {
+        setLoading(true);
+
         axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
             const data = res.data.map((user: User): UserProfile => ({
                 id: user.id,
@@ -24,15 +21,28 @@ export default function App() {
                 address: `${user.address.city} ${user.address.suite} ${user.address.street}`,
             }));
             setUserProfile(data);
+        }).catch(() => {
+            setError(true);
+        }).finally(() => {
+            setLoading(false);
         });
     };
 
     return (
         <div className="App">
             <button type="button" onClick={onClickFetchUser}>データ取得</button>
-            {userProfiles.map((user: UserProfile) => (
-                <UserCard key={user.id} user={user} />
-            ))}
+            <br />
+            {error ? (
+                <p style={{ color: "red" }}>データの取得に失敗しました</p>
+            ) : loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    {userProfiles.map((user: UserProfile) => (
+                        <UserCard key={user.id} user={user} />
+                    ))}
+                </>
+            )}
         </div>
     );
 }
