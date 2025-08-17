f8a1fd00a5f1142e05cd573ce498d76bcc9cf010
Soh1121 <satou.shg@gmail.com>
2025-08-18 06:47:12 +0900

    ✨feat: コンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { UserCard } from "./components/UserCard";
 import "./styles.css";
 
+const user = {
+    id: 1,
+    name: "Soh",
+    email: "123@example.com",
+    address: "ADDRESS"
+};
+
 export default function App() {
+    const on
+
     return (
         <div className="App">
-            <h1>Hello, React!</h1>
-            <p>Welcome to your React application.</p>
+            <UserCard user={user} />
         </div>
     );
 }
