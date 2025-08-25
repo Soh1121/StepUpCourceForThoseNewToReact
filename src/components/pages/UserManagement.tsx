70d12d088e881af0a35c9ff326d2a8fe1e626863
Soh1121 <satou.shg@gmail.com>
2025-08-26 06:54:39 +0900

    ✨feat: ユーザー一覧画面を作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, FC } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
-    return <p>ユーザー管理ページです</p>;
+    const imageUrl="https://plus.unsplash.com/premium_photo-1661892088256-0a17130b3d0d?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    return (
+        <Wrap p={{ base: 4, md: 10 }}>
+            <WrapItem>
+                <UserCard imageUrl={imageUrl} userName="Soh" fullName="フルネーム"></UserCard>
+            </WrapItem>
+        </Wrap>
+    );
 });
