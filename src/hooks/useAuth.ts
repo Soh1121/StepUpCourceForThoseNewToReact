92263f25b36ffe2e87632e461fada8f7e823d392
Soh1121 <satou.shg@gmail.com>
2025-08-25 14:04:05 +0900

    ✨feat: ログイン機能を実装

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useCallback, useState } from "react"
+import { User } from "../components/types/api/user";
+import { useHistory } from "react-router-dom";
+
+export const useAuth = () => {
+    const history = useHistory();
+
+    const [loading, setLoading] = useState(false);
+
+    const login = useCallback((id: string) => {
+        setLoading(true);
+
+        axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
+            if (res.data) {
+                history.push("/home");
+            } else {
+                alert("ユーザーが見つかりません");
+            }
+        })
+        .catch(() => alert("ログインできません"))
+        .finally(() => setLoading(false));
+    }, [history]);
+
+    return { login, loading }
+};
